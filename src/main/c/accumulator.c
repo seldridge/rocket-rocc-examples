df91f1f3c6addf80f607daaf698d61a8d7dcf422
Schuyler Eldridge <schuyler.eldridge@gmail.com>
2016-10-25 16:49:36 -0400

    Fix accumulator.c bug (wrong function signature)

--- src/main/c/accumulator.c
+++ src/main/c/accumulator.c
@@ -14,7 +14,7 @@ uint64_t doRead(uint16_t rocc_rd) {
   return y;
 }
 
-uint64_t doLoad(uint16_t rocc_rd, uint64_t mem_addr) {
+uint64_t doLoad(uint16_t rocc_rd, void * mem_addr) {
   uint64_t y;
   ROCC_INSTRUCTION_R_R_R(XCUSTOM_ACC, y, mem_addr, rocc_rd, k_DO_LOAD);
   return y;
